a743c02f9d095c3b3f9b20bcd8547bed7b3591d9
Hariharan Subramanian <hari@flowinsights.com>
2023-03-26 12:54:53 +0200

    Created Approuter with basic components and fallback error page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,19 @@ import './App.css';
 import {AppHeader} from "./modules/layout/AppHeader";
 import {ThemeProvider} from "@mui/material";
 import {darkTheme} from "./AppTheme";
+import {Route, Routes} from "react-router-dom";
+import {AllPostsPage} from "./modules/blogPosts/AllPostsPage";
+import {SinglePostPage} from "./modules/blogPosts/SinglePostPage";
 
 function App() {
     return (
         <ThemeProvider theme={darkTheme}>
             <div className="App">
                 <AppHeader/>
+                <Routes>
+                    <Route index element={<AllPostsPage/>}/>
+                    <Route path="/posts/:id" element={<SinglePostPage/>}/>
+                </Routes>
             </div>
         </ThemeProvider>
     )
---
+++ frontend/src/modules/blogPosts/SinglePostPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const SinglePostPage = () => {
+    return <>SinglePostView</>;
+};
